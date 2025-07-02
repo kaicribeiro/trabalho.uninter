dd4e5694144fd7cd3e96c1a6459f8bdc2fa1b420
kaicribeiro <kaic68707@gmail.com>
2025-07-01 23:31:23 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+function mostrarMensagem() {
+    const mensagem = document.getElementById("mensagem");
+    mensagem.textContent = "O conhecimento abre portas! Nunca pare de aprender e evoluir. 💡🚀";
+}
